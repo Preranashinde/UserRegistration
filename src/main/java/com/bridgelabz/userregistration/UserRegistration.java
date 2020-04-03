d981f1c3adf65ed7ea3c91b69a82359cd4686999
Preranashinde <preranapshinde3009@gmail.com>
2020-04-03 15:50:39 -0300

    "Validation of email id"

--- src/main/java/com/bridgelabz/userregistration/UserRegistration.java
+++ src/main/java/com/bridgelabz/userregistration/UserRegistration.java
@@ -3,11 +3,14 @@ package com.bridgelabz.userregistration;
 import java.util.regex.Pattern;
 public class UserRegistration {
     String namePattern="^[A-Z][a-zA-Z]{2,}$";
-
+    String emailPattern="^[a-zA-Z]+([+]?[-]?[.]?[_]?[a-zA-Z0-9]+)?[@]{1}[a-z]+[.]{1}[a-z]{2,}([.]?[a-z]+)?$";
     public static void main(String[] args) {
         System.out.println("Welcome To User Registration");
     }
     public boolean checkName(String name) {
         return Pattern.matches(namePattern,name);
+    }
+    public boolean checkEmail(String email) {
+        return Pattern.matches(emailPattern,email);
     }
 }
